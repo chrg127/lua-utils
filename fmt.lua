fca8a803a08ea6b44ed89761216e269e37e2a8f9
chrg <christian.ricci2000@gmail.com>
2024-05-25 14:39:43 +0200

    fmt: add hex(), patch_globals(); modify style
    - Added hex(), to provide a sister function to bin() while also removing
      string.format use for format()
    - Added patch_globals() for substitutig print() and tostring() with the
      library's provided functions
    - Modified some style and indentation to make sure most code is behind
      80 columns
    - Added a few fixes to make sure format() also runs on LuaJIT

--- fmt.lua
+++ fmt.lua
@@ -1,5 +1,8 @@
 local fmt = {}
 
+local old_tostring = tostring
+local old_print = print
+
 local function is_nan(n) return n ~= n end
 local function is_inf(n) return n == math.huge or n == -math.huge end
 
@@ -16,17 +19,18 @@ function fmt.format_table(t, opts)
     opts.indent = opts.indent or 0
     opts.depth = opts.depth or 0
     local line_end = (opts.indent ~= 0 and "\n" or "")
-    local next = next
     if next(t) == nil then
         return spaces(opts) .. "{}"
     end
-    local s = (opts.show_ptr and tostring(t) .. " " or "") .. "{" .. line_end
+    local s = (opts.show_ptr and old_tostring(t) .. " " or "")
+            .. "{" .. line_end
     opts.depth = opts.depth + 1
     local ss = spaces(opts)
     for k, v in pairs(t) do
-        local key = type(k) == "string" and k or "[" .. fmt.pystr_opts(opts, k) .. "]"
+        local key = type(k) == "string"
+                  and k or "[" .. fmt.str_opts(opts, k) .. "]"
         s = s .. ss .. key .. " = "
-              .. (v == t and '(self)' or fmt.pystr_opts(opts, v))
+              .. (v == t and '(self)' or fmt.str_opts(opts, v))
               .. ", " .. line_end
     end
     s = s:sub(1, -3) .. line_end
@@ -34,32 +38,40 @@ function fmt.format_table(t, opts)
     return s .. spaces(opts) .. "}"
 end
 
-function fmt.pystr_opts(opts, ...)
+function fmt.str_opts(opts, ...)
     local args = pack(...)
     local s = ""
     for i = 1, args.n do
-        if type(args[i]) == "table" then
-            s = s .. fmt.format_table(args[i], opts) .. " "
-        else
-            s = s .. tostring(args[i]) .. " "
-        end
+        s = s .. (type(args[i]) == "table"
+                  and fmt.format_table(args[i], opts) .. " "
+                  or  old_tostring(args[i]) .. " ")
     end
     return s:sub(1, -2)
 end
 
-function fmt.pyprint_opts(opts, ...)
-    print(fmt.pystr_opts(opts, ...))
-end
-
-function fmt.pystr(...) return fmt.pystr_opts(nil, ...) end
-function fmt.pyprint(...) return fmt.pyprint_opts(nil, ...) end
+function fmt.print_opts(opts, ...) old_print(fmt.str_opts(opts, ...)) end
+function fmt.tostring(...) return fmt.str_opts(nil, ...) end
+function fmt.print(...) return fmt.print_opts(nil, ...) end
 
 function fmt.bin(n)
     local s = ""
     while n > 0 do
         local digit = n % 2
-        s = tostring(digit) .. s
+        s = (digit == 0 and "0" or "1") .. s
         n = math.floor(n / 2)
+    end
+    return s
+end
+
+function fmt.hex(n, upper)
+    local s = ""
+    local t = upper and { "0", "1", "2", "3", "4", "5", "6", "7",
+                          "8", "9", "A", "B", "C", "D", "E", "F" }
+                    or  { "0", "1", "2", "3", "4", "5", "6", "7",
+                          "8", "9", "a", "b", "c", "d", "e", "f" }
+    while n > 0 do
+        s = s .. t[(n % 16) + 1]
+        n = math.floor(n / 16)
     end
     return s
 end
@@ -130,10 +142,6 @@ local function parse_format_string(s)
         return " ", nil
     end
 
-    local function width()
-        return match("{") and argument() or collect_num(0)
-    end
-
     local function precision()
         return match("{") and argument() or collect_num()
     end
@@ -159,13 +167,13 @@ local function parse_format_string(s)
 
     local function format_spec()
         local r = {}
-        r.fill, r.align  = align()
-        r.sign           = (match("+") or match("-") or match(" ")) and prev or nil
-        r.alternate_conv = match("#")
-        r.zero_pad       = match("0")
-        r.width          = width()
-        r.precision      = match(".") and precision() or nil
-        r.typ, r.case    = typ()
+        r.fill, r.align = align()
+        r.sign          = (match("+") or match("-") or match(" ")) and prev or nil
+        r.alt           = match("#")
+        r.zero_pad      = match("0")
+        r.width         = match("{") and argument() or collect_num(0)
+        r.precision     = match(".") and precision() or nil
+        r.typ, r.case   = typ()
         return r
     end
 
@@ -174,7 +182,7 @@ local function parse_format_string(s)
         local r = {}
         local pos = arg_pos()
         local spec = match(":") and format_spec() or {
-            fill = " ", width = 0, alternate_conv = false
+            fill = " ", width = 0, alt = false
         }
         spec.pos = pos
         consume("}", "expected } before end of format string, found " .. cur .. " instead")
@@ -185,40 +193,40 @@ local function parse_format_string(s)
     return r, i
 end
 
-function fmt.pyformat(fmtstr, ...)
+function fmt.format(fmtstr, ...)
     local args, res, n, i = pack(...), "", 1, 1
 
     local function format_num(spec, num)
         function prefix(if_upper, if_lower)
-            return (spec.alternate_conv and spec.case == "upper") and if_upper
-                or  spec.alternate_conv and if_lower or ""
+            return (spec.alt and spec.case == "upper") and if_upper
+                or  spec.alt and if_lower or ""
         end
         if spec.precision ~= nil and spec.typ ~= nil and spec.typ ~= "scientific"
             and spec.typ ~= "fixed" and spec.typ ~= "general" then
             error("precision not allowed in format specifier '" .. spec.typ .. "'")
         end
         if spec.typ == "decimal" then
-            return tostring(num)
+            return old_tostring(num)
         elseif spec.typ == "binary" then
             return prefix("0B", "0b") .. fmt.bin(num)
         elseif spec.typ == "hex" then
-             return prefix("0X", "0x") .. string.format(spec.case == "upper" and "%X" or "%x", num)
+             return prefix("0X", "0x") .. fmt.bin(num, spec.case == "upper")
         elseif spec.typ == "octal" then
             return prefix("0O", "0o") .. string.format("%o", num)
         elseif spec.typ == "scientific" then
-            return string.format("%" .. (spec.alternate_conv and "#" or "")
+            return string.format("%" .. (spec.alt and "#" or "")
                                      .. "." .. (spec.precision == nil and 6 or spec.precision)
                                      .. (spec.case == "upper" and "E" or "e"), num)
         elseif spec.typ == "fixed" then
             return (spec.case == "upper" and is_nan(num)) and "NAN"
                 or (spec.case == "upper" and is_inf(num)) and "INF"
-                or string.format("%" .. (spec.alternate_conv and "#" or "")
+                or string.format("%" .. (spec.alt and "#" or "")
                                      .. "." .. (spec.precision == nil and 6 or spec.precision)
                                      .. "f", num)
         elseif spec.typ == "general" or spec.typ == nil then
             return (spec.case == "upper" and is_nan(num)) and "NAN"
                 or (spec.case == "upper" and is_inf(num)) and "INF"
-                or string.format("%" .. (spec.alternate_conv and "#" or "")
+                or string.format("%" .. (spec.alt and "#" or "")
                                      .. "." .. (spec.precision == nil and 6 or spec.precision)
                                      .. (spec.case == "upper" and "G" or "g"), num)
         elseif spec.typ == "percent" then
@@ -241,13 +249,13 @@ function fmt.pyformat(fmtstr, ...)
         if arg < 0 then
             error("character code out of range: " .. num)
         else
-            return utf8.char(arg)
+            return utf8 ~= nil and utf8.char(arg) or string.char(arg)
         end
     end
 
     -- this is an extension
     local function format_table(spec, arg)
-        return fmt.format_table(arg, spec.alternate_conv
+        return fmt.format_table(arg, spec.alt
             and { indent = spec.width == 0 and 4 or spec.width }
             or  {})
     end
@@ -261,7 +269,7 @@ function fmt.pyformat(fmtstr, ...)
                or arg_type == "number" and format_num(spec, math.abs(arg))
                or arg_type == "string" and format_string(spec, arg)
                or arg_type == "table" and format_table(spec, arg)
-               or fmt.pystr(arg)
+               or fmt.tostring(arg)
         if spec.sign ~= nil and arg_type ~= "number" then
             error("sign can't be used with argument '" .. arg .. "' of type '" .. arg_type .. "'")
         end
@@ -283,7 +291,7 @@ function fmt.pyformat(fmtstr, ...)
         elseif spec.align == ">" then
             return string.rep(spec.fill, spec.width - #s - #sign) .. sign .. s
         elseif spec.align == "^" then
-            local fill = string.rep(spec.fill, (spec.width - #s - #sign)//2)
+            local fill = string.rep(spec.fill, math.floor((spec.width - #s - #sign)/2))
             return fill .. sign .. s .. fill
         elseif spec.align == "=" then
             return sign .. string.rep(spec.fill, spec.width - #s - #sign) .. s
@@ -328,6 +336,11 @@ function fmt.pyformat(fmtstr, ...)
         i = i + 1
     end
     return res
+end
+
+function fmt.patch_globals()
+    _G.tostring = fmt.tostring
+    _G.print = fmt.print
 end
 
 return fmt
