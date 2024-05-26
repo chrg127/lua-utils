2ea828157faf4451248056b6a3015e7506178b48
chrg <christian.ricci2000@gmail.com>
2024-05-26 10:47:02 +0200

    util: Add any(), all() and comments

--- util.lua
+++ util.lua
@@ -1,5 +1,6 @@
 local util = {}
 
+-- Loop over every byte of a string using for.
 function util.charbytes(s)
     function iter(s, i)
         i = i + 1
@@ -11,6 +12,7 @@ function util.charbytes(s)
     return iter, s, 0
 end
 
+-- Loop over every UTF-8 codepoint of a string using for.
 function util.chars(s)
     local fn = utf8.codes(s)
     function iter(s, i)
@@ -20,6 +22,26 @@ function util.chars(s)
         end
     end
     return iter, s, 0
+end
+
+-- Returns true if any value of `t` is true.
+function util.any(t)
+    for _, v in ipairs(t) do
+        if v then
+            return true
+        end
+    end
+    return false
+end
+
+-- Returns true if all values of `t` are true.
+function util.all(t)
+    for _, v in ipairs(t) do
+        if not v then
+            return false
+        end
+    end
+    return true
 end
 
 return util
